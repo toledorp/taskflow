f5cd361afdd888e8ee368b367c896e0b46743081
toledorp <roge.pupo.toledo@gmail.com>
2026-09-01 20:27:02 -0300

    feat ajuste

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,42 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { Image, Pressable, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { styles } from './styles';
 
-
 export default function Home() {
+    function iniciarAplicacao() {
+        console.log("O botão foi pressionado!")
+    }
     return (
         <SafeAreaView style={styles.safeArea}>
             <View style={styles.container}>
-                <Image
-                    source={require("../assets/images/logo.png")}
-                    style={styles.logo}
-                    resizeMode='contain'
-                />
-                <Text style={styles.titulo}>TaskFlow</Text>
-                <Text style={styles.descricao}>
-                    Organize sua tarefa de frma simples
-                </Text>
+                <View style={styles.card}>
+                    <Image
+                        source={require("../assets/images/logo.png")}
+                        style={styles.logo}
+                        resizeMode='contain'
+                    />
+                    <Text style={styles.titulo}>TaskFlow</Text>
+
+                    <Text style={styles.descricao}>
+                        Organize sua tarefas de forma simples
+                    </Text>
+
+                    <Pressable
+                        onPress={iniciarAplicacao}
+                        style={({ pressed }) => [styles.botao,
+                        pressed && styles.botaoPressionado
+                        ]}
+                    >
+
+                        {({ pressed }) => (
+                            <Text style={styles.textoBotao}>
+                                {pressed ? "Carregando" : "Iniciar"}
+                            </Text>
+                        )}
+
+                    </Pressable>
+                </View>
             </View>
         </SafeAreaView>
     );
-};
-
-
+}
--- app/styles.tsx
+++ app/styles.tsx
@@ -8,19 +8,48 @@ export const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        backgroundColor: '#f3f3f3',
+        padding: 20
     },
     titulo:{
-        fontSize: 32,
+        fontSize: 34,
         fontWeight: "bold",
-        color: '#2c61b6ff'
+        color: '#2c61b6ff',
+        marginBottom: 10
     },
     descricao:{
         fontSize: 18,
         textAlign: "center",
-        marginTop: 15
+        marginBottom: 20,
+        lineHeight: 26,
+        color: '#555'
     },
     logo:{
         width: 140,
         height: 140,
-    }
+        marginBottom: 20
+    }, 
+    card:{
+        backgroundColor: '#ffffff',
+        padding: 30,
+        borderRadius: 20,
+        alignItems: 'center'
+    },
+    botao:{
+        backgroundColor: '#2563EB',
+        paddingVertical: 15,
+        paddingHorizontal: 40,
+        borderRadius: 10
+    },
+    textoBotao:{
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 18
+    },
+    botaoPressionado:{
+        opacity: 0.7,
+        backgroundColor: '#e71212ff',
+        transform: [{scale: 1.5}]
+    },
+
 });
